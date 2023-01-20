7cc3b01988e6050bdc273ce0867cbf1bb512e4b5
MrHipsterJesus <120730450+MrHipsterJesus@users.noreply.github.com>
2023-01-20 13:16:08 -0500

    Update conf.lua
    fixed crash

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.title = "Save The Ball!"
-    t.window.icon = "icon/ball.jpg"
+    --t.window.icon = "icon/ball.jpg"
     t.window.width = 1080
     t.window.height = 720
     t.window.resizable = true
